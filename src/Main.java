17cde36e5143ff03f85794ed1da6e41bbd6b4322
unknown <winyciusyuri@gmail.com>
2026-06-11 14:45:23 -0300

    feat: trabalhando com abstração e encapsulamento, com as classes Curso e Mentoria criadas.

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,19 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
-void main() {
-    //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-    // to see how IntelliJ IDEA suggests fixing it.
-    IO.println(String.format("Hello and welcome!"));
+import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Mentoria;
 
-    for (int i = 1; i <= 5; i++) {
-        //TIP Press <shortcut actionId="Debug"/> to start debugging your code. We have set one <icon src="AllIcons.Debugger.Db_set_breakpoint"/> breakpoint
-        // for you, but you can always add more by pressing <shortcut actionId="ToggleLineBreakpoint"/>.
-        IO.println("i = " + i);
+import java.time.LocalDate;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Curso curso1 = new Curso("curso Java", "descrição curso Java", 8);
+        Curso curso2 = new Curso("curso Js", "descrição curso Js", 4);
+
+        Mentoria mentoria = new Mentoria("mentoria de Java", "descrição mentoria de Java", LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+        System.out.println(mentoria);
     }
 }
