90bbd5c1f9f02fde1dfa37678a2dcb6753b7ea02
unknown <winyciusyuri@gmail.com>
2026-06-16 15:16:06 -0300

    feat: Regra de negócio primcipal criada no Main.java, concluindo o projeto exemplificado no curso

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,6 @@
+import br.com.dio.desafio.dominio.Bootcamp;
 import br.com.dio.desafio.dominio.Curso;
+import br.com.dio.desafio.dominio.Dev;
 import br.com.dio.desafio.dominio.Mentoria;
 
 import java.time.LocalDate;
@@ -6,14 +8,61 @@ import java.time.LocalDate;
 public class Main {
 
     public static void main(String[] args) {
-        Curso curso1 = new Curso("curso Java", "descrição curso Java", 8);
-        Curso curso2 = new Curso("curso Js", "descrição curso Js", 4);
+        Curso curso1 = new Curso();
+        curso1.setTitulo("curso Java");
+        curso1.setDescricao("descrição curso Java");
+        curso1.setCargaHoraria(8);
 
-        Mentoria mentoria = new Mentoria("mentoria de Java", "descrição mentoria de Java", LocalDate.now());
+        Curso curso2 = new Curso();
+        curso2.setTitulo("curso Js");
+        curso2.setDescricao("descrição curso Js");
+        curso2.setCargaHoraria(4);
 
-        System.out.println(curso1);
+        Mentoria mentoria = new Mentoria();
+        mentoria.setTitulo("mentoria de Java");
+        mentoria.setDescricao("descrição mentoria de Java");
+        mentoria.setData(LocalDate.now());
+
+        /*System.out.println(curso1);
         System.out.println(curso2);
+        System.out.println(mentoria); */
 
-        System.out.println(mentoria);
+        Bootcamp bootcamp = new Bootcamp();
+        bootcamp.setNome("Bootcamp Java Developer");
+        bootcamp.setDescricao("Descrição Bootcamp Java Developer");
+        bootcamp.getConteudos().add(curso1);
+        bootcamp.getConteudos().add(curso2);
+        bootcamp.getConteudos().add(mentoria);
+
+        Dev devYuri = new Dev();
+        devYuri.setNome("Yuri");
+
+        Dev devWinycius = new Dev();
+        devWinycius.setNome("Winycius");
+
+        devYuri.inscreverBootcamp(bootcamp);
+        devWinycius.inscreverBootcamp(bootcamp);
+
+        System.out.println("Conteúdos incritos de Yuri: " + devYuri.getConteudosInscritos());
+
+        System.out.println("Conteúdos incritos de Winycius: " + devWinycius.getConteudosInscritos());
+        System.out.println("~~~~~~~~~~~~~~~~~~~~");
+
+        devYuri.progredir();
+        devYuri.progredir();
+
+        devWinycius.progredir();
+        devWinycius.progredir();
+        devWinycius.progredir();
+
+        System.out.println("Conteúdos concluidos de Yuri: " + devYuri.getConteudosConcluidos());
+        System.out.println("Conteúdos concluidos de Winycius: " + devWinycius.getConteudosConcluidos());
+        System.out.println("*-*-*");
+
+        System.out.println("Conteúdos incritos de Yuri: " + devYuri.getConteudosInscritos());
+        System.out.println("XP: " + devYuri.calcularXP());
+
+        System.out.println("Conteúdos incritos de Winycius: " + devWinycius.getConteudosInscritos());
+        System.out.println("XP: " + devWinycius.calcularXP());
     }
 }
